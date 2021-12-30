3980728741ecc6167101f2abd783f2a0d541939a
Your Namenguyenvandong0798 <you@example.comnguyenvandong0798@gmail.com>
2021-12-30 23:22:42 +0700

    commit

--- src/s9_AOP/exersice/bt/src/main/java/com/codegym/bt/aspect/BookAspect.java
+++ src/s9_AOP/exersice/bt/src/main/java/com/codegym/bt/aspect/BookAspect.java
@@ -12,11 +12,27 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class BookAspect {
+    static int browserBook = 0;
+    static int returnBook = 0;
     private Logger logger = LoggerFactory.getLogger(this.getClass());
-    @After("execution(* com.codegym.bt.controller.BookController.*(..))")
-    public void logAfterMethodReturn(JoinPoint joinPoint){
+
+    @AfterThrowing("execution(* com.codegym.bt.controller.BookController.*(..))")
+    public void logAfterMethodReturn(JoinPoint joinPoint) {
         String nameMethod = joinPoint.getSignature().getName();
-//        System.out.println(nameMethod);
-        logger.error("ten phuong thuc "+ nameMethod);
+        logger.error("method name is: " + nameMethod);
+    }
+
+    @After("execution(* com.codegym.bt.controller.BookController.*(..))")
+    public void countLogAfterMethodReturn(JoinPoint joinPoint) {
+        String nameMethod = joinPoint.getSignature().getName();
+        if (nameMethod.equals("browser")) {
+            System.out.println("method name is: " + nameMethod);
+            System.out.println("browser times: " + ++browserBook);
+        } else if (nameMethod.equals("returnBook")) {
+            System.out.println("method name is: " + nameMethod);
+            System.out.println("return times: " + ++returnBook);
+        } else {
+            logger.error("method name is: " + nameMethod);
+        }
     }
 }
